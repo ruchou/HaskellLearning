c2af6ca82e9223d31d9dc027e59a65b39754f67f
Ru Chou (YAN-RU JHOU) <joe04024@gapp.nthu.edu.tw>
2019-06-03 22:55:31 +0800

    Module part 1

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,9 @@ main = do
       let myList = "monkey"
       print . intersperse '.'  $ myList
       print $ all (>2) [4..10]
-
+      print . take 10 . iterate (*2) $ 1
+      print . takeWhile (>3) $ [20,19..1]
+      print . sum . takeWhile (<10000) . map (^5) $ [1..]
 
 
 
