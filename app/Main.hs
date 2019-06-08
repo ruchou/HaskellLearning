39c9c299d660f6d37a51fbcf88e10ba206d4f4f7
Ru Chou (YAN-RU JHOU) <joe04024@gapp.nthu.edu.tw>
2019-06-08 21:44:24 +0800

    Module part 2

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,8 @@ module Main where
 
 import Lib
 import System.IO
-import Data.List (nub, sort, intersperse)
+import Data.List
+--import Data.List (nub, sort, intersperse, group)
 
 main :: IO ()
 
@@ -13,6 +14,14 @@ main = do
       print . take 10 . iterate (*2) $ 1
       print . takeWhile (>3) $ [20,19..1]
       print . sum . takeWhile (<10000) . map (^5) $ [1..]
+      print . dropWhile (<3) $ [1,2,2,2,3,4,5,4,3,2,1]
+      let stock = [(994.4,2008,9,1),(995.2,2008,9,2),(999.2,2008,9,3),(1001.4,2008,9,4),(998.3,2008,9,5)]
+      let ans = head $ dropWhile (\(val,y,m,d)-> val < 1000) stock
+      print ans
+      print $ span (<3) [1..20]
+      print $ map (\l@(x:xs)->(x,length l)) . group . sort $ [1,1,1,1,2,2,2,2,3,3,2,2,2,5,6,7]
+      print $ intersect [1..10] [5..8]
+
 
 
 
