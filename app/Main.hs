c4903f6c034b04a28399411979c14ec01c5b4cd7
Ru Chou (YAN-RU JHOU) <joe04024@gapp.nthu.edu.tw>
2019-06-23 16:42:37 +0800

    haskell tutorial part 10

--- app/Main.hs
+++ app/Main.hs
@@ -6,14 +6,19 @@ import Data.List
 
 --import Data.List (nub, sort, intersperse, group)
 
-reverseWords :: String -> String
-reverseWords = unwords . map reverse . words
+--reverseWords :: String -> String
+--reverseWords = unwords . map reverse . words
+
+fibonacci :: Int -> Int
+fibonacci n
+    | n==0 = 0
+    | n==1 = 1
+    | otherwise = fibonacci (n-1) + fibonacci (n-2)
 
 main :: IO ()
 main = do
-    a <- return "hello"
-    b <- return "world"
-    print $ a ++ " " ++ b
+    let ten = fibonacci 10
+    print ten
 
 
 
