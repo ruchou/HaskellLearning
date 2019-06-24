825629d1c997f81d74729de94deb562595f9c2eb
Ru Chou (YAN-RU JHOU) <joe04024@gapp.nthu.edu.tw>
2019-06-24 23:12:43 +0800

    haskell tutorial part 17

--- app/Main.hs
+++ app/Main.hs
@@ -4,70 +4,13 @@ import Lib
 import System.IO
 import Data.List
 
---import Data.List (nub, sort, intersperse, group)
-
---reverseWords :: String -> String
---reverseWords = unwords . map reverse . words
-
-fibonacci :: Int -> Int
-fibonacci n
-    | n==0 = 0
-    | n==1 = 1
-    | otherwise = fibonacci (n-1) + fibonacci (n-2)
-
---check :: Bool -> String
---check isTrue = case isTrue of True -> "Yes"
---                              False -> "No"
-fibonacciLt :: Int -> [Int]
-fibonacciLt n = [fibonacci x | x <- [0 .. n]]
-    where fibonacci n
-              | n == 0 = 0
-              | n == 1 = 1
-              | otherwise = fibonacci (n-1) + fibonacci(n-2)
-
-isRightTriangle :: Float -> Float -> Float -> Bool
-isRightTriangle = \a -> \b -> \c -> a**2+b**2 == c**2
-
-showAbsSumOf :: [Int] -> String
-showAbsSumOf = show . abs . sum
-
-swap :: (a,b)->(b,a)
-swap (a,b) = (b,a)
-
-
-
-password :: String -> Maybe String
-password person = lookupUsers [("bill","1234")]
-        where
-                lookupUsers [] = Nothing
-                lookupUsers ((name, passwd):xs) =
-                        if name == person then Just passwd
-                         else lookupUsers xs
-
-class Comp a
-        where comp:: a -> a -> Integer
-
-instance Comp Integer
-        where comp x y = x - y
-
-quickSort:: [Int] -> [Int]
-quickSort [] = []
-quickSort (x:xs) = quickSort small ++ [x] ++ quickSort large
-                where small = [y | y <-xs, y < x]
-                      large = [y | y <-xs , y>=x]
-
-data Rectangle = Rec {
-                        x::Int
-                       ,y::Int
-                       ,width::Int
-                       ,height::Int
-                       } deriving Show
 
 
 main :: IO ()
 main = do
-        let myRect = Rec 2 3 4 5
-        print$ myRect
+        print "hello"
+        print "world"
+        someFunc
 
 
 
