d6385c7d170e1185164ce2a8fb7deafd9048179b
yanrujhou <yanrujhou@deepforce.com>
2019-06-27 09:47:52 +0800

    learn haskell part 20

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,17 @@ import Lib
 import System.IO
 import Data.List
 import MyModule
+import Text.Printf
 
+hanoi 1 a _ c = printf "Move from %c to %c\n" a c
+hanoi n a b c = do
+        hanoi (n-1) a c b
+        hanoi 1 a b c
+        hanoi (n-1) b a c
 
 main :: IO ()
 main = do
-        print "hello"
-        print "world"
-        let s1 = fromList [1,2,3,4,3,2,1]
-        print $ s1
+        hanoi 5 'a' 'b' 'c'
 
 
 
