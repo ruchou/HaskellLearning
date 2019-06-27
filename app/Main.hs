54f9452d483624c7c1d27c53bae9305635e45a2c
yanrujhou <yanrujhou@deepforce.com>
2019-06-27 16:32:25 +0800

    learn haskell part 23

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,22 @@
 module Main where
 
-import Lib
-import System.IO
-import Data.List
-import MyModule
-import Text.Printf
-import Control.Monad
-import Data.List
+import Data.Typeable
+import Prelude hiding (catch)
 import Control.Exception
+import System.Environment
+import System.IO
 
-
-myHead :: [a] -> Either String a
-myHead [] = Left "an empty list has no head .. XD"
-myHead (x:xs) = Right x
 
 main :: IO ()
-main = do
-    result <- try (  evaluate $ head [])
-    case result :: Either SomeException Int of
-            Left ex -> putStrLn $ "exception"
-            Right ele -> putStrLn . show $ ele
+main = catch
+        ( do
+                (fileName:_) <- getArgs
+                contents <- readFile fileName
+                putStrLn contents
+        )
+        (\(SomeException e) -> do print $ typeOf e
+                                  print  e
+        )
 
 
 
