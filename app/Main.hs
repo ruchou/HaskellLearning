191b758796d4466639025c68ae8891eccf393a0f
yanrujhou <yanrujhou@deepforce.com>
2019-06-27 14:19:25 +0800

    learn haskell part 21

--- app/Main.hs
+++ app/Main.hs
@@ -6,15 +6,23 @@ import Data.List
 import MyModule
 import Text.Printf
 
-hanoi 1 a _ c = printf "Move from %c to %c\n" a c
-hanoi n a b c = do
-        hanoi (n-1) a c b
-        hanoi 1 a b c
-        hanoi (n-1) b a c
+
+echoUntil :: String -> IO()
+echoUntil str = do
+        input <- getLine
+        if input /= str
+            then do
+                putStrLn (">> " ++ input)
+                echoUntil str
+            else
+                return ()
+
 
 main :: IO ()
 main = do
-        hanoi 5 'a' 'b' 'c'
+        echoUntil "test"
+
+
 
 
 
