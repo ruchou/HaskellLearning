289da6ed4130f01a3a643b92e2120108cd6abf38
yanrujhou <yanrujhou@deepforce.com>
2019-06-28 13:51:29 +0800

    learn haskell part 24

--- app/Main.hs
+++ app/Main.hs
@@ -6,17 +6,13 @@ import Control.Exception
 import System.Environment
 import System.IO
 
-myTry :: Exception e => IO a -> IO (Either e a)
-myTry a = catch toEither (return . Left)
-        where toEither = do
-                r <- a
-                return (Right r)
 
-
+myLast :: [a] -> a
+myLast [] = error "this is an error"
+myLast (x:[]) = x
+myLast (x:xs) = myLast xs
 
 main :: IO ()
 main = do
-        result <- myTry $ evaluate $ head []
-        case result :: Either SomeException Int of
-                Left ex -> putStrLn "exception"
-                Right ele -> putStrLn $ show ele
+        let ele = myLast [1..10]
+        putStrLn . show $ ele
