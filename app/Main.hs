17c2c004bf15e37fb1dbf45d52e6ba1b4b7b9b61
yanrujhou <yanrujhou@deepforce.com>
2019-06-28 14:28:23 +0800

    learn haskell part 25

--- app/Main.hs
+++ app/Main.hs
@@ -7,15 +7,9 @@ import System.Environment
 import System.IO
 
 
-data ListException = EmptyListException String deriving (Show, Typeable)
-
-instance Exception ListException
-
-init' :: [a] ->  [a]
-init' [] = throw $ EmptyListException "an empty list has no init"
-init' (x:[]) = []
-init' (x:xs) = x : init' xs
-
 main :: IO ()
 main = do
-       init' []
+       result <- fmap (show .(*2).read ) getLine
+       putStrLn result
+
+
