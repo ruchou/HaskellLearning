a049d9960c191ff5aec6fa586f0be071a40afb80
yanrujhou <yanrujhou@deepforce.com>
2019-06-28 14:54:56 +0800

    learn haskell part 26

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,18 @@ import Control.Exception
 import System.Environment
 import System.IO
 
+myPrint :: Show a => Maybe a -> IO ()
+myPrint (Just x) = print x
+myPrint n        = print n
+
 
 main :: IO ()
 main = do
-       result <- fmap (show .(*2).read ) getLine
-       putStrLn result
+       let m1 = fmap (+1) (Just 3)
+       let m2 = fmap (*3) m1
+       let m3 = fmap show m2
+       myPrint m3
+       let m4 = fmap (show . (*3) . (+1)) (Just 3)
+       myPrint m4
 
 
