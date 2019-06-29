47a7dbc3f665e50617b8222a54bae64ec16e0231
Ru Chou (YAN-RU JHOU) <joe04024@gapp.nthu.edu.tw>
2019-06-30 00:41:57 +0800

    haskell tutorial part 28

--- app/Main.hs
+++ app/Main.hs
@@ -10,11 +10,5 @@ import Control.Applicative
 
 main :: IO ()
 main = do
-       let a = ["1234","4567","12389"]
-       let b = ["joe","bill","cartier"]
-       let a_b = (++) <$> a <*> b
-       print $ a_b
-       let fs = pure (+) <*> [1..5]
-       print $ fs <*> [10..15]
-       let myList = filter (>6) $ (+) <$> [1..3] <*> [1..6]
-       print myList
+       let a = (:) <$> Just 1 <*> Just [2..3]
+       print a
